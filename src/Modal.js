9d288436bc447e1e808942c81fde2dae332b3db1
Jayson Wu <wulingjie991@outlook.com>
2020-11-08 12:11:31 +0800

    upgrade to hooks

--- src/Modal.js
+++ src/Modal.js
@@ -1,68 +1,27 @@
-import React from 'react'
+import React, { useRef, useState, useLayoutEffect } from 'react'
 
-import {
-  MODAL_BACKDROP,
-  MODAL,
-  MODAL_DIALOG,
-  MODAL_CONTENT,
-  MODAL_HEADER,
-  MODAL_TITLE,
-  MODAL_CLOSE,
-  MODAL_CLOSE_HOVER,
-  MODAL_BODY
-} from './ModalStyle'
+import { MODAL_BACKDROP, MODAL, MODAL_DIALOG, MODAL_CONTENT, MODAL_HEADER, MODAL_TITLE, MODAL_CLOSE, MODAL_CLOSE_HOVER, MODAL_BODY } from './ModalStyle'
 
-class Modal extends React.Component {
-  constructor(props) {
-    super(props)
+const Modal = ({ title, showModal, onHideModal, children }) => {
+  const modalContent = useRef(null)
+  const [ hover, setHover ] = useState(false)
+  const [ marginTop, setMarginTop ] = useState(60)
 
-    this.modalContent = null
+  useLayoutEffect(() => {
+    fixMarginTop()
+    window.addEventListener('resize', fixMarginTop)
+  }, [])
 
-    this.state = {
-      hover: false,
-      marginTop: 60
-    }
-
-    this.toggleHover = this.toggleHover.bind(this)
-    this.fixMarginTop = this.fixMarginTop.bind(this)
-    this.getClientWidthHeight = this.getClientWidthHeight.bind(this)
-  }
-
-  componentDidMount() {
-    this.fixMarginTop()
-    window.addEventListener('resize', this.fixMarginTop)
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('resize', this.fixMarginTop)
-  }
-
-  getElementPosition() {
-    const defaultRect = { top: 0, left: 0 }
-
-    const rect =
-      (this.modalContent.current && this.modalContent.current.getBoundingClientRect()) ||
-      defaultRect
-
-    return {
-      top: rect.top + document.body.scrollTop,
-      left: rect.left + document.body.scrollLeft
-    }
-  }
-
-  getClientWidthHeight() {
+  const getClientWidthHeight = () => {
     const width =
-      (this.modalContent && this.modalContent.current && this.modalContent.current.clientWidth) || 0
+      (modalContent && modalContent.current && modalContent.current.clientWidth) || 0
     const height =
-      (this.modalContent && this.modalContent.current && this.modalContent.current.clientHeight) ||
+      (modalContent && modalContent.current && modalContent.current.clientHeight) ||
       0
-    return {
-      width,
-      height
-    }
+    return { width, height }
   }
 
-  getViewportOffset() {
+  const getViewportOffset = () => {
     if (window.innerWidth) {
       return {
         width: window.innerWidth,
@@ -83,53 +42,41 @@ class Modal extends React.Component {
     }
   }
 
-  fixMarginTop() {
-    if (this.getClientWidthHeight().height > this.getViewportOffset().height) {
-      this.setState({
-        marginTop: 30
-      })
+  const fixMarginTop = () => {
+    if (getClientWidthHeight().height > getViewportOffset().height) {
+      setMarginTop(30)
     } else {
-      this.setState({
-        marginTop: this.getViewportOffset().height / 2 - this.getClientWidthHeight().height / 2 - 60
-      })
+      setMarginTop(getViewportOffset().height / 2 - getClientWidthHeight().height / 2 - 60)
     }
   }
 
-  toggleHover() {
-    this.setState({
-      hover: !this.state.hover
-    })
-  }
-
-  render() {
-    return (
-      <div style={{ visibility: this.props.showModal ? 'visible' : 'hidden' }}>
-        <div style={MODAL_BACKDROP}></div>
-        <div tabIndex={-1} role='dialog' onClick={this.props.onHideModal} style={MODAL}>
-          <div role='document' style={MODAL_DIALOG}>
-            <div
-              ref={this.modalContent}
-              style={{ ...MODAL_CONTENT, marginTop: this.state.marginTop }}
-            >
-              <div style={MODAL_HEADER}>
-                <h5 style={MODAL_TITLE}>{this.props.title ? this.props.title : ''}</h5>
-                <button
-                  type='button'
-                  aria-label='Close'
-                  style={this.state.hover ? { ...MODAL_CLOSE, ...MODAL_CLOSE_HOVER } : MODAL_CLOSE}
-                  onMouseEnter={this.toggleHover}
-                  onMouseLeave={this.toggleHover}
-                >
-                  <span aria-hidden='true'>&times;</span>
-                </button>
-              </div>
-              <div style={MODAL_BODY}>{this.props.children}</div>
+  return (
+    <div style={{ visibility: showModal ? 'visible' : 'hidden' }}>
+      <div style={MODAL_BACKDROP}></div>
+      <div tabIndex={-1} role='dialog' onClick={onHideModal} style={MODAL}>
+        <div role='document' style={MODAL_DIALOG}>
+          <div
+            ref={modalContent}
+            style={{ ...MODAL_CONTENT, marginTop }}
+          >
+            <div style={MODAL_HEADER}>
+              <h5 style={MODAL_TITLE}>{title || ''}</h5>
+              <button
+                type='button'
+                aria-label='Close'
+                onMouseEnter={() => setHover(!hover)}
+                onMouseLeave={() => setHover(!hover)}
+                style={hover ? { ...MODAL_CLOSE, ...MODAL_CLOSE_HOVER } : MODAL_CLOSE}
+              >
+                <span aria-hidden='true'>&times;</span>
+              </button>
             </div>
+            <div style={MODAL_BODY}>{children}</div>
           </div>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default Modal
